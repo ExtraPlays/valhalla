77bb52f08c10a6dafc137aedb5f9da61310d05d7
Vitor Barbosa <extraplays324@gmail.com>
2024-07-23 17:25:29 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,13 @@
 Config = {}
 
+-- Padrão para criação de mapas 
+--["praça"] = {                     Nome do mapa
+--  ["spawn"] = {                   Tabela de posições das equipes
+--    attackers = { x = -1037.0, y = -2737.0, z = 13.0 },
+--    defenders = { x = -1037.0, y = -2737.0, z = 13.0 }
+--  },
+--  maxPlayers = 10,                Quantidade maxima de jogadores por mapa
+--},
 Config.mapas = {
   ["praça"] = {
     ["spawn"] = {
